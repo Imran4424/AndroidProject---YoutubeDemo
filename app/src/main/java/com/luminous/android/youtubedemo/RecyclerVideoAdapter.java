71f42c61e8b8ca79cab8affbdf4271dde204af1d
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-22 12:19:13 +0600

    recycler video adapter

--- app/src/main/java/com/luminous/android/youtubedemo/RecyclerVideoAdapter.java
+++ app/src/main/java/com/luminous/android/youtubedemo/RecyclerVideoAdapter.java
@@ -1,4 +1,32 @@
 package com.luminous.android.youtubedemo;
 
-public class RecyclerVideoAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerVideoAdapter extends RecyclerView.Adapter<RecyclerVideoAdapter.VideoViewHolder> {
+    @NonNull
+    @Override
+    public VideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class VideoViewHolder extends RecyclerView.ViewHolder {
+
+        public VideoViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
