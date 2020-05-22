bd20de375b80a9535e4d1496699d94a057446d7c
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-22 16:10:54 +0600

    youtube videos

--- app/src/main/java/com/luminous/android/youtubedemo/VideoRecyclerAdapter.java
+++ app/src/main/java/com/luminous/android/youtubedemo/VideoRecyclerAdapter.java
@@ -1,5 +1,7 @@
 package com.luminous.android.youtubedemo;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,9 +12,13 @@ import java.util.List;
 
 public class VideoRecyclerAdapter extends RecyclerView.Adapter<VideoRecyclerAdapter.ViewHolder> {
 
+    private final Context context;
+    private final LayoutInflater layoutInflater;
     List<YouTubeVideos> youTubeVideosList;
 
-    public VideoRecyclerAdapter(List<YouTubeVideos> youTubeVideosList) {
+    public VideoRecyclerAdapter(Context context, LayoutInflater layoutInflater, List<YouTubeVideos> youTubeVideosList) {
+        this.context = context;
+        this.layoutInflater = layoutInflater;
         this.youTubeVideosList = youTubeVideosList;
     }
 
