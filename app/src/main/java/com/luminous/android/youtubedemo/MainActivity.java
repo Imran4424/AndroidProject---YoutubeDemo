472898f65ca3db20067296ddcec4a46b558a4f2c
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-24 01:41:00 +0600

    Recycler View

--- app/src/main/java/com/luminous/android/youtubedemo/MainActivity.java
+++ app/src/main/java/com/luminous/android/youtubedemo/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,9 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String[] titles = getResources().getStringArray(R.array.lip_sync_battle_title);
+        List<String> titleList = Arrays.asList(titles);
+
         String[] links = getResources().getStringArray(R.array.lip_sync_battle_links);
+        List<String> linkList = 
 
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         final LinearLayoutManager listLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(listLayoutManager);
+
+        final VideoRecyclerAdapter videoRecyclerAdapter = new VideoRecyclerAdapter(this, titleList, links);
     }
 }
