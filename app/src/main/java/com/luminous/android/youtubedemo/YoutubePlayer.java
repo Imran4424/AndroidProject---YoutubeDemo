c6ec4281b8deec3ba469c946f00ba0ad0c8785c0
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-24 11:17:36 +0600

    youtube player

--- app/src/main/java/com/luminous/android/youtubedemo/YoutubePlayer.java
+++ app/src/main/java/com/luminous/android/youtubedemo/YoutubePlayer.java
@@ -1,8 +1,13 @@
 package com.luminous.android.youtubedemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class YoutubePlayer extends AppCompatActivity {
     public static String youtubeVideoId;
@@ -11,5 +16,16 @@ public class YoutubePlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube_player);
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = youtubeVideoId;
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
     }
 }
